034350e7594fdb424a58b41f7571e477b3b9bce0
Wedrig <jackkmorice@gmail.com>
2016-12-09 16:26:06 -0600

    Create page.js

---
+++ page.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<div class="nav"><a href="//personalhealth.github.io"><li>Personal Health</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//frostbird.github.io/frosty.css">'
