6629b0ecae3c8f444c388f5c801fb13cdf803e58
Wedrig <jackkmorice@gmail.com>
2017-01-08 20:02:15 -0600

    Update page.js

--- page.js
+++ page.js
@@ -1,2 +1,2 @@
-document.body.innerHTML='<div class="nav"><a href="//personalhealth.github.io"><li>Personal Health</li></a></div> \
-<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//frostbird.github.io/frosty.css">'
+document.body.innerHTML='<div class="nav"><a href="//personalhealth.github.io"><li>personalhealth</li></a><a href="//personalhealth.github.io/index.html"><li>index</li></a></div> \
+<div class="c">'+document.body.innerHTML+'</div><link rel="stylesheet" type="text/css" href="//personalhealth.github.io/hygeine.css">'
